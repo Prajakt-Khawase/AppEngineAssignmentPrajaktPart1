c7ec08806946417853e40d1603c1569ca0b42f45
Prajakt Uttamrao Khawase <khawse.prajakt@gmail.com>
2019-11-16 14:14:40 -0600

    Added the comments to the code

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
 let express = require('express')
 let app = express()
 let url = require('url')
-
 let timesHelloed = 0;
 
+// get method for root call
 app.get('/', (req, res) => {
-      
-      res.send('Welcome to the homepage\n')
+      res.send('Welcome to the homepage\n') // Sending the response to browser.
     })
 
+// get method if the request comes with path /hello
 app.get('/hello', (req, res) => {
-    timesHelloed++;
-    res.send("value of timesHelloed  is " + timesHelloed)
+    timesHelloed++; // Incrementing the timesHelloed variable by 1.
+    res.send("value of timesHelloed  is " + timesHelloed) // Sending the response to browser.
 })
 
+// get method if the request comes with path /resetTimesHelloed
 app.get('/resetTimesHelloed', (req, res) => {
-    timesHelloed = 0;
-    res.send("value of timesHelloed has been reseted to " + timesHelloed)
+    timesHelloed = 0; // Setting the timesHelloed variable to zero.
+    res.send("value of timesHelloed has been reseted to " + timesHelloed) // Sending the response to browser.
 })
 
+// get method if the request comes with path /timesHelloed
 app.get('/timesHelloed', (req, res) => {
     res.send('/ hello is called ' + timesHelloed +' number of times.')
 })
 
+// Configuring the port value to 4001
 let port = process.env.PORT || 3001
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
